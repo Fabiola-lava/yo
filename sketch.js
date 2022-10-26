1f4c564adea937afc38592aa30daed43fb27cc30
Fabiola-lava <116762900+Fabiola-lava@users.noreply.github.com>
2022-10-26 15:59:28 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+function setup() {
+  createCanvas(600, 200);
+  var caja = createSprite (100,100, 20,20);
+caja.shapeColor = "purple"
+  
+}
+function draw() {
+  background(180);
+  
+  
+  drawSprites();
+}
